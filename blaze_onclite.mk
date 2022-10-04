c31a0148ca4c832bfdf25fd4a63a911d05522e24
afterallafk <shivamatiet2001@gmail.com>
2022-10-04 13:34:09 +0530

    ProjectBlaze Bringup
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

--- blaze_onclite.mk
+++ blaze_onclite.mk
@@ -12,14 +12,24 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit from onclite device
 $(call inherit-product, device/xiaomi/onclite/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common ProjectBlaze stuff.
+$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
+
+# Blaze Official Stuff
+BLAZE_BUILD_TYPE := OFFICIAL
+BLAZE_MAINTAINER := Aditya-&-Abhinav
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_USE_PIXEL_CHARGER := true
+TARGET_SUPPORTS_BLUR := true
+
+# Boot Animation
+TARGET_BOOT_ANIMATION_RES := 720
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_onclite
+PRODUCT_NAME := blaze_onclite
 PRODUCT_DEVICE := onclite
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := Redmi 7
+PRODUCT_MODEL := Redmi Y3
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_SYSTEM_NAME := onc
 
